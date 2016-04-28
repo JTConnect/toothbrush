e68473c46f095482f1af63cc2c9c9780e671c0f3
James Rolon <Jrolon@Jamess-MacBook-Pro.local>
2016-04-28 00:36:58 -0400

    Changed link logic in preview controller

--- app/main/createJob/previewJob/previewJob.controller.js
+++ app/main/createJob/previewJob/previewJob.controller.js
@@ -7,19 +7,16 @@
 
     function PreviewJobController(CreateJobService,$state) {
         var vm = this;
-        vm.message = "Perfect! Last Step";
-        vm.previewJob = CreateJobService.GetJobPosting();
 
         activate();
+
         function activate() {
-            //redirect user if job posting is null
-            if(vm.previewJob == null || vm.previewJob == undefined) {$state.go('root.appLayout.createJob.postAJob'); }
-            iLink(vm.previewJob.CompanyUrl);
-        }
-        function iLink(url){ 
-            var createLink = 'http://' + url; 
-            document.getElementById('clickableUrl').innerHTML += '<a href="'+ createLink +'">' + url +'</a>'; 
-            vm.previewJob.CompanyUrl = createLink; 
+            vm.message = "Perfect! Last Step";
+            vm.previewJob = CreateJobService.GetJobPosting();
+
+            if(!vm.previewJob) {$state.go('root.appLayout.createJob.postAJob'); }
+
+            vm.previewJob.CompanyUrl = "http://" + vm.previewJob.CompanyUrl;
         }
     }
 })();
